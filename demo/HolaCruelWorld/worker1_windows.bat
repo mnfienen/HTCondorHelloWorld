b8a4591b8da5b2dd54aa3213ba375d7a32a56ad0
Mike Fienen <mnfienen@usgs.gov>
2017-11-07 15:57:13 -0600

    Made windows version

---
+++ demo/HolaCruelWorld/worker1_windows.bat
@@ -0,0 +1,16 @@
+# grab the number passed in from the master
+set run_number=%1
+
+
+# keeping our focus
+SqUiRReL!!!!!!!!!!!
+
+# uncompress the data folder and jump into it
+unzip data.zip
+cd data
+
+# run python script with the number passed in
+python cruel.py %run_number%
+
+
+
